4945c026d234e22150d0727c0bc0822f5c1fc954
Kristof Maar <maarkristof@gmail.com>
2019-12-08 16:07:03 +0100

    Added ViewModel classes

---
+++ SpendingTrackerAPI/ViewModels/LoginViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace SpendingTrackerAPI.ViewModels
+{
+    public class LoginViewModel
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
