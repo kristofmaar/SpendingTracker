9d1855540af5a3b996e054402d4cf1ad2b502ed8
Kristof Maar <maarkristof@gmail.com>
2019-12-09 20:45:40 +0100

    Added base entity controllers

---
+++ SpendingTrackerAPI/Controllers/CategoryController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using SpendingTrackerAPI.Data;
+using SpendingTrackerAPI.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SpendingTrackerAPI.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        private readonly UserManager<User> _userManager;
+        private readonly IConfiguration _configuration;
+        private readonly SpendingTrackerDbContext _context;
+
+        public CategoryController(UserManager<User> userManager, IConfiguration configuration, SpendingTrackerDbContext context)
+        {
+            _userManager = userManager;
+            _configuration = configuration;
+            _context = context;
+        }
+
+        [HttpGet]
+        [Route("list/{guid}")]
+        public ActionResult<IEnumerable<Category>> GetAllByUserId(string guid)
+        {
+            return _context.Categories.Where(e => e.UserId == new Guid(guid)).ToList();
+        }
+
+        [HttpPost]
+        public void Post([FromBody]Category newCategory)
+        {
+            _context.Categories.Add(newCategory);
+        }
+
+        [HttpPost]
+        public void Update([FromBody]Category updatedCategory)
+        {
+            _context.Categories.Update(updatedCategory);
+        }
+    }
+}
---
+++ SpendingTrackerAPI/Controllers/SpendingController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using SpendingTrackerAPI.Data;
+using SpendingTrackerAPI.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SpendingTrackerAPI
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SpendingController : ControllerBase
+    {
+        private readonly UserManager<User> _userManager;
+        private readonly IConfiguration _configuration;
+        private readonly SpendingTrackerDbContext _context;
+
+        public SpendingController(UserManager<User> userManager, IConfiguration configuration, SpendingTrackerDbContext context)
+        {
+            _userManager = userManager;
+            _configuration = configuration;
+            _context = context;
+        }
+
+        [HttpGet]
+        [Route("list/{guid}")]
+        public ActionResult<IEnumerable<Spending>> GetAllByUserId(string guid)
+        {
+            return _context.Spendings.Where(e => e.UserId == new Guid(guid)).ToList();
+        }
+
+        [HttpPost]
+        public void Post([FromBody]Spending newSpending)
+        {
+            _context.Spendings.Add(newSpending);
+        }
+
+        [HttpPost]
+        public void Update([FromBody]Spending updatedSpending)
+        {
+            _context.Spendings.Update(updatedSpending);
+        }
+    }
+}
